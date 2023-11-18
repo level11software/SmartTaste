6711421212f8e28d8e392f15e3609be38ea5f9c4
Alessandro Lisi <61741415+PrivateAbstractAleLisi@users.noreply.github.com>
2023-11-18 23:13:44 +0100

    animations and opacity in diet selection

--- frontend/hello-client/src/components/Quiz/DietQuiz.jsx
+++ frontend/hello-client/src/components/Quiz/DietQuiz.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import DietGlassCard from "./SubComponents/DietGlassCard.jsx";
+import { motion } from "framer-motion";
 
 const DietPyramidPage = ({onContinue}) => {
-    const [diet, setDiet] = useState(null);
+    const [selectedDiet, setSelectedDiet] = useState(null);
+
+    const handleDietChange = (diet) => {
+        console.log("Diet level changed to:", diet);
+        setSelectedDiet(diet);
+
+    };
 
 
     return (
@@ -15,21 +22,29 @@ const DietPyramidPage = ({onContinue}) => {
             </div>
 
                 <div className="grid grid-cols-2 grid-rows-2 gap-8 ">
-            <DietGlassCard onDietChange={setDiet} title={"Vegan"}
+            <DietGlassCard onDietChange={handleDietChange} title={"Vegan"}
                            description={"bravo vegano."}
-                           image_src={"https://cdn-prod.medicalnewstoday.com/content/images/articles/324/324343/plant-meal.jpg"}/>
+                           image_src={"https://cdn-prod.medicalnewstoday.com/content/images/articles/324/324343/plant-meal.jpg"}
+                           isSelected={selectedDiet === "Vegan"}
+                           selectedDiet={selectedDiet}/>
 
-            <DietGlassCard onDietChange={setDiet} title={"Vegetarian"}
+            <DietGlassCard onDietChange={handleDietChange} title={"Vegetarian"}
                            description={"one that does not include any meat or seafood."}
-                           image_src={"https://www.verywellfit.com/thmb/ZivTDxsx-dBBYR8_iTQmKjJ9JDo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-diet-crop-f35bdf38c6c14ba3a189bc3900dd3384.jpg"}/>
+                           image_src={"https://www.verywellfit.com/thmb/ZivTDxsx-dBBYR8_iTQmKjJ9JDo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/vegetarian-diet-crop-f35bdf38c6c14ba3a189bc3900dd3384.jpg"}
+                           isSelected={selectedDiet === "Vegetarian"}
+                           selectedDiet={selectedDiet}/>
 
-            <DietGlassCard onDietChange={setDiet} title={"Pescatarian"}
+            <DietGlassCard onDietChange={handleDietChange} title={"Pescatarian"}
                            description={"don't give up your dose of Omega 3 fatty acids!"}
-                           image_src={"https://static.wixstatic.com/media/2b1bc2_a869d0bd4f6345588ca333a83c5b3c61~mv2.jpg/v1/fill/w_640,h_426,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/2b1bc2_a869d0bd4f6345588ca333a83c5b3c61~mv2.jpg"}/>
+                           image_src={"https://static.wixstatic.com/media/2b1bc2_a869d0bd4f6345588ca333a83c5b3c61~mv2.jpg/v1/fill/w_640,h_426,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/2b1bc2_a869d0bd4f6345588ca333a83c5b3c61~mv2.jpg"}
+                           isSelected={selectedDiet === "Pescatarian"}
+                           selectedDiet={selectedDiet}/>
 
-            <DietGlassCard onDietChange={setDiet} title={"Omnivore"}
+            <DietGlassCard onDietChange={handleDietChange} title={"Omnivore"}
                            description={"as nature made you."}
-                           image_src={"https://www.worldanimalprotection.org.uk/sites/default/files/styles/600x400/public/media/1019368_0.jpg?itok=AzTD9_S_"}/>
+                           image_src={"https://www.worldanimalprotection.org.uk/sites/default/files/styles/600x400/public/media/1019368_0.jpg?itok=AzTD9_S_"}
+                           isSelected={selectedDiet === "Omnivore"}
+                           selectedDiet={selectedDiet}/>
 
                 </div>
             <div className="p-4 justify-center">
