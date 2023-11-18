1742c2fe96bd6c257de3c68ed11fff56ca14fc2c
Gabriele Marra <61741485+gabrielemarra@users.noreply.github.com>
2023-11-18 18:20:29 +0100

    fixed interaction

--- backend/db/models/interaction.py
+++ backend/db/models/interaction.py
@@ -1,22 +1,24 @@
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from enum import Enum
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Enum as SQLEnum
 
 from db.models.base import Base
 
 
 # Define an enumeration for the InteractionType
-class InteractionTypeEnum(str, Enum):
-    view = "view"
-    like = "like"
-    comment = "comment"
-    save = "save"
+class InteractionTypeEnum(Enum):
+    OPENED_RECIPE = "opened_recipe"
+    BOUGHT_RECIPE = "bought_recipe"
+    DISCARDED_RECIPE = "discarded_recipe"
 
 
 class Interaction(Base):
     __tablename__ = "interactions"  # the name of the table in the database
     id = Column(Integer, primary_key=True, index=True)  # the primary key of the table
     time = Column(String)  # timestamp or datetime for the interaction time
-    # eventType = Column(Enum(InteractionTypeEnum), nullable=False)  # eventType as ENUM
+    eventType = Column(SQLEnum(InteractionTypeEnum))
     recipeID = Column(Integer, ForeignKey('recipes.id'))  # foreign key to recipeID
     userID = Column(Integer, ForeignKey('users.id'))  # foreign key to userID
 
